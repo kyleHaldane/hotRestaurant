c9b566dda8be61bec9fa74fa6820190fac3a1429
Avery Stahl <averystahl@hotmail.com>
2020-01-09 10:40:52 -0500

    server.js psuedocode  / commented out steps to make server

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies: express, path
+
+// Setup express app
+
+// Setup Port
+
+// Setup express app data parsing
+
+// Create array to store reservations
+
+// Create array to store waitlist
+
+// Be able to push user data from form into reservations array
+
+// If the reservations array has 5 elements then push to waitlist array
+
+// Routes
+// Route to main page
+
+// Route to reservation form
+
+// Route to view tables
+
+// Start server
+
+
